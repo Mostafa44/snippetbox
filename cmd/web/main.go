a8ea27343c74da06ff123bf6fe5a4712acbd53f7
Mostafa44 <mostafa.abdelshafy88@gmail.com>
2026-06-27 02:39:31 +0300

    provide the structured-loggers to the handlers via dependency injection by defining the handlers as methods to a struct containing the logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+// Define an application struct to hold the application-wide dependencies for the web application.
+
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -14,17 +20,21 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home) // restrict this route to exact matches on / only
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", app.home) // restrict this route to exact matches on / only
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	logger.Info("Starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
