daa9202cbe0938991cf55e31783c7385dcb868b1
Mostafa44 <mostafa.abdelshafy88@gmail.com>
2026-06-27 01:18:31 +0300

    Add structred log using log/slog

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -22,8 +26,8 @@ func main() {
 
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("Starting server on %s", *addr)
+	logger.Info("Starting server", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
-
+	logger.Error(err.Error())
+	os.Exit(1)
 }
