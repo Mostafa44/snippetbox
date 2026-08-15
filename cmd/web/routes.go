0728df1937e0f9c331eecb9e781b81db2a8952f3
Mostafa44 <mostafa.abdelshafy88@gmail.com>
2026-08-16 00:25:52 +0300

    CSRF Protection

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,7 +13,7 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home)) // restrict this route to exact matches on / only
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
