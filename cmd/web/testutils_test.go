b85621af4a5722d796c89dcf3c25888f87786058
Mostafa44 <mostafa.abdelshafy88@gmail.com>
2026-09-12 18:46:06 +0300

    Using testing-helpers and account for cookies and redirects

---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestApplication(t *testing.T) *application {
+	return &application{
+		logger: slog.New(slog.DiscardHandler),
+	}
+}
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ts.Client().Jar = jar
+	// Prevent the client from following redirects automatically by setting a
+	//custom CheckRedirect function. This function runs whenever a 3xx
+	// response is received. By returning http.ErrUseLastResponse, we tell the client
+	// the client to stop and immediately return the received response.
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	return &testServer{ts}
+}
+
+func (ts *testServer) resetClientCookieJar(t *testing.T) {
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ts.Client().Jar = jar
+}
+
+type testResponse struct {
+	status  int
+	headers http.Header
+	cookies []*http.Cookie
+	body    string
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) testResponse {
+	req, err := http.NewRequest(http.MethodGet, ts.URL+urlPath, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	res, err := ts.Client().Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return testResponse{
+		status:  res.StatusCode,
+		headers: res.Header,
+		cookies: res.Cookies(),
+		body:    string(bytes.TrimSpace(body)),
+	}
+}
